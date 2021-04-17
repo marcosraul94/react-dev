6d2fe06684f1e0a3e4f079b307469cbb5ae1034d
git <marcosraul94@gmail.com>
2021-04-17 00:00:42 -0400

    Updates

--- src/app.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from 'src/App';
-
-it('Test', () => {
-  const { getByText } = render(<App />);
-  getByText('Hello World');
-});
